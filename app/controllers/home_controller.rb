2658dd7c1aa3e925420996336b9150354a643f7e
Jkuerzel <jkuerzel@chicagobooth.edu>
2024-12-05 17:47:58 +0000

    Introduced Profit to Buildings table on landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,8 @@ class HomeController < ApplicationController
 
     @the_resource=Resource.where({:id=>10}).at(0)
     @the_building=Building.where({:id=>10}).at(0)
-    @list_of_maps=Map.all
+    @list_of_maps=Map.all.order({:operating_profit=>:desc})
+    @top10_maps = @list_of_maps.first(10)
     render({:template=>"home_templates/login"})
   end
 end

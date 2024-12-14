5c2b90d6baafc3fb7f99cab1fb5a695d89451c86
Jkuerzel <jkuerzel@chicagobooth.edu>
2024-12-14 16:09:44 +0000

    Bug Fix on landing page (when no maps exist)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,10 @@
 class HomeController < ApplicationController
   def index
 
-    @the_resource=Resource.where({:id=>10}).at(0)
-    @the_building=Building.where({:id=>10}).at(0)
-
     render({:template=>"home_templates/index"})
   end
   def login
 
-    @the_resource=Resource.where({:id=>10}).at(0)
-    @the_building=Building.where({:id=>10}).at(0)
     @list_of_maps=Map.all.order({:operating_profit=>:desc})
     @top10_maps = @list_of_maps.first(10)
     render({:template=>"home_templates/login"})
